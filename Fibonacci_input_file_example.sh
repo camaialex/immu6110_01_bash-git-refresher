c1dc340e68b569dc2143b52452c128cdcf54aca6
camaialex <alex.e.camai@gmail.com>
2026-09-16 15:25:51 -0600

    fibonacci update

---
+++ Fibonacci_input_file_example.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# for loop - version 2
+for i in $(cat Fibonacci_sequence.txt);
+do
+  echo "The value of variable i in version 2 of the for loop is ${i}."
+done
+
+echo "The script is finished running, goodbye!"
